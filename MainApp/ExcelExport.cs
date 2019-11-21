d990ccf13051baf457f8918e5c18cfdac7ac1dce
purkayasta <pritompurkayasta@gmail.com>
2019-11-22 00:06:31 +0600

    Excel Export

---
+++ MainApp/ExcelExport.cs
@@ -0,0 +1,44 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System.Data;
+
+namespace MainApp
+{
+    static class ExcelExport
+    {
+        public static void GenerateExcel(DataTable dataTable, string path)
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.Tables.Add(dataTable);
+
+            // create a excel app along side with workbook and worksheet and give a name to it
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook excelWorkBook = excelApp.Workbooks.Add();
+            Excel._Worksheet xlWorksheet = excelWorkBook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            foreach (DataTable table in dataSet.Tables)
+            {
+                //Add a new worksheet to workbook with the Datatable name
+                Excel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
+                excelWorkSheet.Name = table.TableName;
+
+                // add all the columns
+                for (int i = 1; i < table.Columns.Count + 1; i++)
+                {
+                    excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+                }
+
+                // add all the rows
+                for (int j = 0; j < table.Rows.Count; j++)
+                {
+                    for (int k = 0; k < table.Columns.Count; k++)
+                    {
+                        excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+                    }
+                }
+            }
+            excelWorkBook.SaveAs(path);
+            excelWorkBook.Close();
+            excelApp.Quit();
+        }
+    }
+}
---
+++ MainApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using MainApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userList = UserManager.SeedData();
+            try
+            {
+                Console.WriteLine("Please select a operation to do ..");
+                Console.WriteLine("1. Export Data as EXCEL");
+                int command = Convert.ToInt32(Console.ReadLine());
+                switch (command)
+                {
+                    case 1:
+                        string fileName = "UserManager.xlsx";
+                        Console.WriteLine("Please give a location to save :");
+                        string location = Console.ReadLine();
+                        string customExcelSavingPath = location + "\\" + fileName;
+                        ExcelExport.GenerateExcel(ConvertToDataTable(userList), customExcelSavingPath);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        // T : Generic Class
+        static DataTable ConvertToDataTable<T>(List<T> models)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            // Loop through all the properties            
+            // Adding Column to our datatable
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property names  
+                dataTable.Columns.Add(prop.Name);
+            }
+            // Adding Row
+            foreach (T item in models)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows  
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                // Finally add value to datatable  
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
+    }
+}
